f6b150df77b0d0f87f7bb70ff6ad717554a652ef
reece-elder <reece.elder@qa.com>
2022-01-06 11:07:13 +0000

    URL Params

--- Express/002-Request-handling/index.js
+++ Express/002-Request-handling/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Making a variable called app that is equal to express
 const app = express();
 
+// Needed to parse JSON data so express can read it
+app.use(express.json());
+
 // A basic get request to return a console log
 // app.METHOD("/path", whatRequestDoes() (as an arrow function))
 app.get("/hello", () => {
@@ -12,6 +15,34 @@ app.get("/hello", () => {
 
 app.delete("/delete", () => {
     console.log("Deleted item!");
+})
+
+app.post("/post", () => {
+    console.log("Item posted :)");
+})
+
+// Requests
+
+// URL Params ID number, package name (Nodemon)
+// Within the path if something has a colon before it that is the url param '/get/reece'
+app.get('/get/:name', (req) => {
+    // console.log(req);
+    console.log(req.params);
+    console.log(req.params.name);
+})
+
+// Query Params - ?species=lizard
+app.get('/get', (req) => {
+    console.log(req.query);
+    console.log(req.query.species);
+})
+
+// Request body - Form data
+app.post('/newSnake', (req, res) => {
+    console.log("========================");
+    const body = req.body;
+    console.log(body);
+    // res.send("hello");
 })
 
 
--- Express/002-Request-handling/index.md
+++ Express/002-Request-handling/index.md
@@ -18,3 +18,19 @@ Add a basic get request to our server that will return some data
 Exercise -
 Create 4 request handlers (GET, POST, PUT, DELETE)
 Each just console logs out "Item added.." 
+
+Requests and Responses
+
+Requests - Client sends us (Search queries, id numbers, body etc)
+Response - Server sends the client (data from the database)
+
+
+Exercise 
+
+Create the 4 basic crud (CREATE, READ, UPDATE, DELETE)
+Each request should console.log out the request
+
+Delete - Takes in an id and prints it out (URL Param)
+Read - Takes in URL Query and prints them out 
+Create - Takes in a body (using postman) and prints it out
+Update - Takes in an id and body, prints them both out (Either way)
