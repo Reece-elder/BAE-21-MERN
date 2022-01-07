d5948f2651ccebb55aa7e2d57127ec9368409d44
reece-elder <reece.elder@qa.com>
2022-01-07 10:35:56 +0000

    connecting to the DB

---
+++ Express/005-mongooseDemo/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+// Importing in the mongoose stuff
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database connection - URI similar to URL, it is a link to a service through the internet
+// Connect to MongoDB with - mongoose.connect(uri, options)
+// mongodb URI - mongodb://localhost:27017/<db_name>
+mongoose.connect('mongodb://localhost:27017/zoo' , {useNewUrlParser: true}, 
+(error) => {
+    if(error) {
+        console.log("Cant connect to DB :( ");
+    } else if (!error) {
+        console.log("Connected to DB :D ");
+    }
+});
+
+// Front end has a form that produces `data`
+// Data is sent through the browser to the backend 
+// `Parse` the data to make it usable / readable to do stuff with it
+
+
+// Listen on port 5015
+const server = app.listen(5015, () => {
+    console.log(`Server started!`);
+});
---
+++ Express/005-mongooseDemo/index.md
@@ -0,0 +1,12 @@
+# Mongoose
+
+Mongoose allows us to Model objects for use with MongoDB (Schemas)
+Allows interaction with DB using query language
+
+npm i mongoose 
+
+# What we're doing with Mongoose
+
+1 - Connecting to our MongoDB in our index.js
+2 - Creating Lizard schemas / objects in a separate file
+3 - Modifying our routes so they post, get, update and delete from the database
