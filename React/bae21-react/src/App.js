ec3e47c8469117c25bd54a2fd45e6bb5e161b109
reece-elder <reece.elder@qa.com>
2022-01-11 12:44:09 +0000

    Skeleton Code - first comp

---
+++ React/bae21-react/src/App.js
@@ -0,0 +1,14 @@
+import HelloWorld from './components/HelloWorld';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Components MUST BEGIN WITH CAPITAL LETTER Ctrl + / to auto comment */}
+      <HelloWorld/>
+      
+    </div>
+  );
+}
+
+export default App;
